108d24b22494c99b1242e45f1d5bef7c2912b69d
AndreasHGK <36563463+AndreasHGK@users.noreply.github.com>
2018-02-27 19:09:27 +0100

    Version 3.0!!
    Finally works now! (actually this time)
    sorry for the delay...

--- src/AndreasHGK/AutoMineReset/Main.php
+++ src/AndreasHGK/AutoMineReset/Main.php
@@ -4,22 +4,21 @@ use pocketmine\command\CommandExecutor;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 use pocketmine\command\CommandSender;
+use pocketmine\command\ConsoleCommandSender;
 use pocketmine\command\Command;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat as C;
-use AndreasHGK\AutoMineReset\TimedReset\ResetMine;
-use falkirks\minereset\Mine;
-use AndreasHGK\AutoMineReset\Timer;
 class Main extends PluginBase{
+	
+	public $paused = false;
+	public $sec = 0;
+	public $autopaused = false;
+	public $interval = 600;
 	
 	public function onLoad(){
 		$this->getLogger()->notice(C::GREEN." Loading...");
-		$paused = false;
-		$autopaused = false;
-		$interval = 600;
-		$sec = 0;
 		$this->saveDefaultConfig();
         	$this->reloadConfig();
 		if(is_int($this->getConfig()->get('reset-time')) == false){
@@ -27,35 +26,34 @@ class Main extends PluginBase{
 				$this->getLogger()->notice(C::BOLD.C::RED."[FATAL]".C::RESET.C::YELLOW." Config is setup incorrectly!");
 			}
 		}
+
+	}
+	
+	public function prepLoop(){
+		global $interval;
+		global $sec;
+		$sec = 1;
 		$interval = $this->getConfig()->get('reset-time');
-	global $paused;
-	global $autopaused;
-	global $interval;
-	global $sec;
+		$milliseconds = 1000;
+		$seconds=(int)$milliseconds/1000;
+			while(true)
+			{
+				$this->update();
+				sleep($seconds);
+			}
 	}
 	
 	public function onEnable(){
-		if(!isset($sec)){
-    	$sec = 0;
-		}
 		$this->getLogger()->notice(C::GREEN." Enabled!");
-		$current_time = time();	
-		
-		$this->setInterval($this->update($this->getConfig()->get('reset-time')),1000);
+		$this->prepLoop();
 	}
 	
-	public function setInterval($f, $milliseconds)
-		{
-			$seconds=(int)$milliseconds/1000;
-			while(true)
-			{
-				$f();
-				sleep($seconds);
-			}
-		}
 	
 	public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
-		if(strtolower($cmd->getName()) == "mr"){
+		global $autopaused;
+		global $paused;
+		switch($command->getName()){
+			case mr:
 			if($sender->hasPermission("minereset.command.resetall")){
 				$this->resetAll();
 				$count = count($this->getApi()->getMineManager());
@@ -64,9 +62,10 @@ class Main extends PluginBase{
 			else{
             $sender->sendMessage(TextFormat::RED . "You do not have permission to run this command." . TextFormat::RESET);
             }
-		} elseif(strtolower($cmd->getName()) == "autoreset"){
+		
+		case autoreset:
 			if($sender->hasPermission("amr.autoreset")){
-				if($paused === true){
+				if($paused == true){
 				$paused = false;
 				$sender->sendMessage(C::BOLD.C::GREEN."Autoreset enabled!");
 				$this->getLogger()->notice(C::GREEN." The timer has been enabled by ".$sender."!");
@@ -79,57 +78,66 @@ class Main extends PluginBase{
 			} else {
             $sender->sendMessage(TextFormat::RED . "You do not have permission to run this command." . TextFormat::RESET);
 		}
-	}
+	}}
 	
-	}
 	public function autostop(){
+		static $y = false;
+		global $paused;
+		global $autopaused;
 			if($this->getConfig()->get('sleep-when-empty') == true){
 				if(count($this->getServer()->getOnlinePlayers()) < 0){
 					if($paused == false){
-						$paused = true;
-						$autopaused = true;
-						$this->getLogger()->notice(C::GREEN." The timer has been auto-disabled!");
-					}
-					} else {
+						if($y == true){
 						$paused = false;
 						$autopaused = false;
+						$y = false;
 						$this->getLogger()->notice(C::GREEN." The timer has been auto-enabled!");
+						}
+					}
+					} else {
+						if($y == false) {
+						$paused = true;
+						$y = true;
+						$autopaused = true;
+						$this->getLogger()->notice(C::GREEN." The timer has been auto-disabled!");
+						}
 					}
 			}
 		}
 	public function resetAll(){
-		$server->dispatchCommand(new ConsoleCommandSender(), 'mine reset-all');
+		$cmd = "mine reset-all";
+		$this->getServer()->dispatchCommand(new ConsoleCommandSender(),$cmd);
 		
-		foreach($this->getServer()->getOnlinePlayers() as $p){
-			$p->sendMessage(C::BOLD.C::RED."All mines have been reset!");
+		Server::getInstance()->broadcastMessage(C::BOLD.C::DARK_RED."[AutoMineReset] ".C::RESET.C::YELLOW." All mines have been reset!");
 		}
-	}
+	
 	
 	public function onDisable(){
 		$this->getLogger()->notice(C::GREEN." Disabled!");
 	}
 	
-	public function autoresettask($in){
-		while($sec >= $in){
-			$this->resetAll();
-		}
-	}
-	public function update($interval2) {
-		if(!isset($interval2)){
-    	$interval = 600;
-	}
-		$this->autoresettask($interval2);
+	public function update() {
 		$this->autostop();
 		$this->bettertimer();
+		global $sec;
+		global $interval;
+		if($sec >= $interval){
+			$this->resetAll();
+		}
 		}
 	
 	public function betterTimer() {
-		if(pause == false){
+		global $interval;
+		global $paused;
+		global $sec;
+		if($paused == false){
 			$sec++;
 		}
 		if($sec > $interval){
-			$sec = 0;
+			$sec = 1;
 		}
+		//$this->getLogger()->notice(C::GREEN." Debug says(sec): ".$sec);
+		//$this->getLogger()->notice(C::GREEN." Debug says(int): ".$interval);
 	}
 	
 }
